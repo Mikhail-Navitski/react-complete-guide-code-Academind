bb3765770efe7b072a19ed8bccb97ae261f7ddeb
KobraMik <slavik433@gmail.com>
2022-06-21 23:00:12 +0300

    03 React Basics & Working With Components - added basic logic to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
